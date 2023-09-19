35e96b6921ee61580e70340744d5c993e35b9b30
marius <ciurel.marius@gmail.com>
2023-09-19 17:20:55 +0300

    cadsa

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from tkinter import *
 from tkinter import messagebox
 from random import randint, shuffle, choice
 import pyperclip
+import json
+
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 def pass_generator():
@@ -11,30 +13,64 @@ def pass_generator():
     numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
     symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
     password_input.delete(0, END)
-    password_list = [choice(letters) for char in range(randint(8, 10))]
-    password_list += [choice(symbols) for char1 in range(randint(2, 4))]
-    password_list += [choice(numbers) for char2 in range(randint(2, 4))]
+    password_list = [choice(letters) for _ in range(randint(8, 10))]
+    password_list += [choice(symbols) for _ in range(randint(2, 4))]
+    password_list += [choice(numbers) for _ in range(randint(2, 4))]
     shuffle(password_list)
     password = "".join(password_list)
     password_input.insert(0, f"{password}")
     pyperclip.copy(password)
 
+
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save():
-    if len(website_input.get()) == 0 or len(password_input.get()) == 0:
+    website = website_input.get()
+    email = user_input.get()
+    password = password_input.get()
+    new_data = {
+        website: {
+            "email": email,
+            "password": password,
+        }
+    }
+    if len(website) == 0 or len(password) == 0:
         messagebox.showerror(title="Oops", message="Please don't leave any fields empty!")
     else:
-        is_ok = messagebox.askokcancel(title=website_input.get(), message=f"These are details entered : \n"
-                                                                          f"Email{user_input.get()}"
-                                                                          f"\nPassword:{password_input.get()}\n"
-                                                                          f"Is it ok to save?")
-        if is_ok:
-            with open("data.txt", "a") as file:
-                file.write(f"{website_input.get()}|{user_input.get()}|{password_input.get()}\n")
+        try:
+            with open("data.json", "r") as data_file:
+                # Reading old data
+                data = json.load(data_file)
+
+        except FileNotFoundError:
+            with open("data.json", "w") as data_file:
+                json.dump(new_data, data_file, indent=4)
+        else:
+            # Updating old data with new data
+            data.update(new_data)
+            with open("data.json", "w") as data_file:
+                # Saving updated data
+                json.dump(data, data_file, indent=4)
+        finally:
             website_input.delete(0, END)
-            user_input.delete(0, END)
-            user_input.insert(0, "-@gmail.com")
             password_input.delete(0, END)
+
+
+# -------------------------- Find Password ---------------------------- #
+def find_password():
+    try:
+        with open("data.json", "r") as data_file:
+            data = json.load(data_file)
+
+    except FileNotFoundError:
+        messagebox.showinfo(title=f'File Not Found', message="Create a file by adding "
+                                                             "an email address or refer to the file path")
+    else:
+        if website_input.get() in data:
+            messagebox.showinfo(title=f" {website_input.get()} ",
+                                message=f"Email :  {data[website_input.get()]['email']} \n"
+                                        f"Password :{data[website_input.get()]['password']}")
+        else:
+            messagebox.showinfo(title="Not Found", message="Website not found in the database!")
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -54,9 +90,9 @@ user_label.grid(row=2, column=0, sticky="E")
 password_label = Label(text="Password", padx=20, pady=5)
 password_label.grid(row=3, column=0, sticky="E")
 # input interface
-website_input = Entry(width=35)
+website_input = Entry(width=21)
 website_input.focus()
-website_input.grid(row=1, column=1, columnspan=2, sticky="W")
+website_input.grid(row=1, column=1, sticky="W")
 user_input = Entry(width=35)
 user_input.insert(0, "-@gmail.com")
 user_input.grid(row=2, column=1, columnspan=2, sticky="W")
@@ -67,4 +103,6 @@ gen_pass_button = Button(text="Generate password", highlightthickness=0, command
 gen_pass_button.grid(row=3, column=2, sticky="W")
 add_button = Button(text="Add", highlightthickness=0, width=36, command=save)
 add_button.grid(row=4, column=1, columnspan=2, sticky="W")
+search_button = Button(text="Search", highlightthickness=0, command=find_password)
+search_button.grid(row=1, column=2, sticky="W")
 window.mainloop()
